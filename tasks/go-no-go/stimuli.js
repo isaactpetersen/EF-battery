91ca9bb6a5dc5ae8b15bbf64f846058a99b990ef
RomainPastureau <romain.pastureau@hotmail.fr>
2023-06-22 17:17:30 +0200

    Go/No-Go: Stimuli for the main task
    Added the stimuli blocks for the main task

--- tasks/go-no-go/stimuli.js
+++ tasks/go-no-go/stimuli.js
@@ -5,33 +5,70 @@ var message_incorrect = "<b><div style='color:red; font-size:30px''>Incorrect</d
 
 //We define the practice stimuli
 //Go condition
-var practice_stimuli = [{
-  //The stimulus: a big square of one of the two colors
-  stimulus: "<div class = centerbox><div id=" + stims[0][1] + "></div></div>",
-  stimulus_after_key_press: "<div class = centerbox><div id=" + stims[0][1] + "-responded></div></div>",
-  data: {
+var practice_stimuli_block = [{
+    //The stimulus: a big square of one of the two colors
+    stimulus: "<div class = centerbox><div id=" + stims[0][1] + "></div></div>",
+    stimulus_after_key_press: "<div class = centerbox><div id=" + stims[0][1] + "-responded></div></div>",
+    data: {
     trial_id: "Practice",
     condition: "Go",
     correct_response: " "
-  },
-  timeout_message: message_incorrect,
-  condition: "Go",
-  key_answer: " "
+    },
+    timeout_message: message_incorrect,
+    condition: "Go",
+    key_answer: " "
 },
 //No-Go condition
 {
-  //The stimulus: a big square of the other color
-  stimulus: "<div class = centerbox><div id=" + stims[1][1] + "></div></div>",
-  stimulus_after_key_press: "<div class = centerbox><div id=" + stims[1][1] + "-responded></div></div>",
-  data: {
+    //The stimulus: a big square of the other color
+    stimulus: "<div class = centerbox><div id=" + stims[1][1] + "></div></div>",
+    stimulus_after_key_press: "<div class = centerbox><div id=" + stims[1][1] + "-responded></div></div>",
+    data: {
     trial_id: "Practice",
     condition: "No-Go",
     correct_response: undefined
-  },
-  timeout_message: message_correct,
-  condition: "No-Go",
-  key_answer: "NO_KEYS"
+    },
+    timeout_message: message_correct,
+    condition: "No-Go",
+    key_answer: "NO_KEYS"
 }];
 
-var practice_trials = jsPsych.randomization.repeat(practice_stimuli, 5); //Put here how many pairs of trials you want in
-                                                                         //the practice trials
+var practice_trials = jsPsych.randomization.repeat(practice_stimuli_block, 5); //Put here how many pairs of trials you
+                                                                               //want in the practice trials
+
+var main_stimuli_block = [];
+var number_of_go_stimuli_per_block = 1;
+var number_of_no_go_stimuli_per_block = 5;
+
+//Go stimuli
+for (var i = 0; i < number_of_go_stimuli_per_block; i++){
+    main_stimuli_block.push({
+        stimulus: "<div class = centerbox><div id=" + stims[0][1] + "></div></div>",
+        stimulus_after_key_press: "<div class = centerbox><div id=" + stims[0][1] + "-responded></div></div>",
+        data: {
+            trial_id: "Practice",
+            condition: "Go",
+            correct_response: " "
+        },
+        condition: "Go",
+        key_answer: " "
+    });
+};
+
+//No-go stimuli
+for (var i = 0; i < number_of_no_go_stimuli_per_block; i++){
+    main_stimuli_block.push({
+        stimulus: "<div class = centerbox><div id=" + stims[1][1] + "></div></div>",
+        stimulus_after_key_press: "<div class = centerbox><div id=" + stims[1][1] + "-responded></div></div>",
+        data: {
+            trial_id: "Practice",
+            condition: "No-Go",
+            correct_response: undefined
+        },
+        condition: "No-Go",
+        key_answer: "NO_KEYS"
+    });
+};
+
+var main_trials = jsPsych.randomization.repeat(main_stimuli_block, 50); //Put here how many blocks you want in the
+                                                                        //whole task
