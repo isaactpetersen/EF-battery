92bcbef9a56bd3d0b59e20400ac0fd121657f6cf
RomainPastureau <romain.pastureau@hotmail.fr>
2023-06-26 17:15:29 +0200

    Go/No-Go: Minor changes (new commit)
    Contains the changes announced in the previous commit: modifies mainly the stimuli so that you can change the length of the blocks from the init.

--- tasks/go-no-go/init.js
+++ tasks/go-no-go/init.js
@@ -1,3 +1,12 @@
+var go_stimuli_block_practice = 1;
+var no_go_stimuli_block_practice = 1;
+var blocks_practice = 5;
+
+var go_stimuli_block_main = 5; //number of go trials per block
+var no_go_stimuli_block_main = 1; //number of no-go trials per block
+var blocks_main = 50; //number of times the test trial blocks are shown
+                      //5 go + 1 no-go x 50 = 300 test trials
+
 //We initialize jsPsych
 var jsPsych = initJsPsych({
     override_safe_mode: true, //We keep this here for test purposes, as we're running the code locally for now. If we remove it, we get a warning that some jsPsych functions only work online.
--- tasks/go-no-go/stimuli.js
+++ tasks/go-no-go/stimuli.js
@@ -4,42 +4,44 @@ var message_correct = "<b><div style='color:green; font-size:30px'>Correct!</div
 var message_incorrect = "<b><div style='color:red; font-size:30px''>Incorrect</div></b>";
 
 //We define the practice stimuli
-//Go condition
-var practice_stimuli_block = [{
-    //The stimulus: a big square of one of the two colors
-    stimulus: "<div class = centerbox><div id=" + stims[0][1] + "></div></div>",
-    stimulus_after_key_press: "<div class = centerbox><div id=" + stims[0][1] + "-responded></div></div>",
-    data: {
-    trial_id: "Practice",
-    condition: "Go",
-    correct_response: " "
-    },
-    timeout_message: message_incorrect,
-    key_answer: " "
-},
-//No-Go condition
-{
-    //The stimulus: a big square of the other color
-    stimulus: "<div class = centerbox><div id=" + stims[1][1] + "></div></div>",
-    stimulus_after_key_press: "<div class = centerbox><div id=" + stims[1][1] + "-responded></div></div>",
-    data: {
-    trial_id: "Practice",
-    condition: "No-Go",
-    correct_response: "NO_KEYS"
-    },
-    timeout_message: message_correct,
-    key_answer: "NO_KEYS"
-}];
+var practice_stimuli_block = [];
 
-var practice_trials = jsPsych.randomization.repeat(practice_stimuli_block, 5); //Put here how many pairs of trials you
-                                                                               //want in the practice trials
+//Go condition
+for (var i = 0; i < go_stimuli_block_practice; i++){
+    practice_stimuli_block.push({
+        stimulus: "<div class = centerbox><div id=" + stims[0][1] + "></div></div>",
+        stimulus_after_key_press: "<div class = centerbox><div id=" + stims[0][1] + "-responded></div></div>",
+        data: {
+            trial_id: "Practice",
+            condition: "Go",
+            correct_response: " "
+        },
+        timeout_message: message_incorrect,
+        key_answer: " "
+    });
+};
+
+//No-go condition
+for (var i = 0; i < go_stimuli_block_practice; i++){
+    practice_stimuli_block.push({
+        stimulus: "<div class = centerbox><div id=" + stims[1][1] + "></div></div>",
+        stimulus_after_key_press: "<div class = centerbox><div id=" + stims[1][1] + "-responded></div></div>",
+        data: {
+            trial_id: "Practice",
+            condition: "No-Go",
+            correct_response: "NO_KEYS"
+        },
+        timeout_message: message_correct,
+        key_answer: "NO_KEYS"
+    });
+};
+
+var practice_trials = jsPsych.randomization.repeat(practice_stimuli_block, blocks_practice);
 
 var main_stimuli_block = [];
-var number_of_go_stimuli_per_block = 5; //number of go trials per block
-var number_of_no_go_stimuli_per_block = 1; //number of no-go trials per block
 
 //Go stimuli
-for (var i = 0; i < number_of_go_stimuli_per_block; i++){
+for (var i = 0; i < go_stimuli_block_main; i++){
     main_stimuli_block.push({
         stimulus: "<div class = centerbox><div id=" + stims[0][1] + "></div></div>",
         stimulus_after_key_press: "<div class = centerbox><div id=" + stims[0][1] + "-responded></div></div>",
@@ -53,7 +55,7 @@ for (var i = 0; i < number_of_go_stimuli_per_block; i++){
 };
 
 //No-go stimuli
-for (var i = 0; i < number_of_no_go_stimuli_per_block; i++){
+for (var i = 0; i < no_go_stimuli_block_main; i++){
     main_stimuli_block.push({
         stimulus: "<div class = centerbox><div id=" + stims[1][1] + "></div></div>",
         stimulus_after_key_press: "<div class = centerbox><div id=" + stims[1][1] + "-responded></div></div>",
@@ -66,5 +68,4 @@ for (var i = 0; i < number_of_no_go_stimuli_per_block; i++){
     });
 };
 
-var main_trials = jsPsych.randomization.repeat(main_stimuli_block, 50); //number of times the test trial blocks are shown
-                                                                        //5 go + 1 no-go x 50 = 300 test trials
+var main_trials = jsPsych.randomization.repeat(main_stimuli_block, blocks_main);
