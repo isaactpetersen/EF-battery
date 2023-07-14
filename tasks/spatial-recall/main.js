b0a59a034578555164368c1f25e30d51377768e2
RomainPastureau <romain.pastureau@hotmail.fr>
2023-07-14 15:38:24 +0200

    Spatial Recall: Get browser and URL info

--- tasks/spatial-recall/main.js
+++ tasks/spatial-recall/main.js
@@ -1,6 +1,7 @@
 const timeline = [];
 
 timeline.push(fullscreen_on);
+timeline.push(get_browser_and_url_info);
 timeline.push(instructions);
 timeline.push(recall_forwards);
 timeline.push(fullscreen_off);
