473f20fa4a39392f25c733d8f3d9e7a9517bb389
RomainPastureau <romain.pastureau@hotmail.fr>
2023-07-15 11:16:07 +0200

    Spatial Recall: Trying HTTP saving option

--- tasks/spatial-recall/init-test.js
+++ tasks/spatial-recall/init-test.js
@@ -6,20 +6,27 @@ var jsPsych = initJsPsych({
     override_safe_mode: true, //We keep this here for test purposes, as we're running the code locally for now. If we remove it, we get a warning that some jsPsych functions only work online.
     on_finish: function() {
         last_trial_data = jsPsych.data.getLastTrialData().trials[0];
-        console.log(last_trial_data);
         file_name = "spatial-recall";
         if ("subid" in last_trial_data){
             file_name += "-" + last_trial_data["subid"];
         };
         file_name += ".csv";
 
-        jsPsych.data.get().localSave("csv", file_name);
-
         current_html = window.location.href.split("/");
         redirect_html = ""
+        
         for (i = 0; i < current_html.length - 1; i++) {
             redirect_html += current_html[i] + "/"
         };
+
+        save_url = redirect_html + "results/save_data.php"
+        data_dir = redirect_html + "results/spatial-recall/"
+
+        if (current_html[0].startsWith("http")) {
+            saveData(save_url, data_dir, file_name);
+        } else if (current_html[0].startsWith("file")) {
+            jsPsych.data.get().localSave("csv", file_name);
+        }
 
         // We add the task to the URL
         redirect_html += "experiment-go-no-go-test.html";
@@ -33,5 +40,19 @@ var jsPsych = initJsPsych({
         };
 
         // We redirect to the next task
-        window.location = redirect_html;    }
+        window.location = redirect_html;    
+    }
 });
+
+function saveData(save_url, data_dir, file_name) {
+    jQuery.ajax({
+        type: 'post',
+        cache: false,
+        url: save_url,
+        data: {
+            data_dir: data_dir,
+            file_name: file_name, // the file type should be added
+            exp_data: jsPsych.data.get().csv()
+        }
+    });
+}
--- tasks/spatial-recall/init.js
+++ tasks/spatial-recall/init.js
@@ -6,14 +6,11 @@ var jsPsych = initJsPsych({
     override_safe_mode: true, //We keep this here for test purposes, as we're running the code locally for now. If we remove it, we get a warning that some jsPsych functions only work online.
     on_finish: function() {
         last_trial_data = jsPsych.data.getLastTrialData().trials[0];
-        console.log(last_trial_data);
         file_name = "spatial-recall";
         if ("subid" in last_trial_data){
             file_name += "-" + last_trial_data["subid"];
         };
         file_name += ".csv";
-
-        jsPsych.data.get().localSave("csv", file_name);
 
         current_html = window.location.href.split("/"); //We get the current URL, and separate all the elements by the "/" symbol
         redirect_html = ""
@@ -22,6 +19,15 @@ var jsPsych = initJsPsych({
             redirect_html += current_html[i] + "/"
         };
 
+        save_url = redirect_html + "results/save_data.php"
+        data_dir = redirect_html + "results/spatial-recall/"
+
+        if (current_html[0].startsWith("http")) {
+            saveData(save_url, data_dir, file_name);
+        } else if (current_html[0].startsWith("file")) {
+            jsPsych.data.get().localSave("csv", file_name);
+        }
+
         // We add the task to the URL
         redirect_html += "experiment-go-no-go.html";
 
@@ -29,7 +35,7 @@ var jsPsych = initJsPsych({
         if((window.location.href).indexOf('?') != -1) {
 
           var variables = window.location.href.split('?')[1]; 
-          redirect_html += variables;
+          redirect_html += "?" + variables;
 
         };
 
@@ -37,3 +43,16 @@ var jsPsych = initJsPsych({
         window.location = redirect_html;
     }
 });
+
+function saveData(save_url, data_dir, file_name) {
+    jQuery.ajax({
+        type: 'post',
+        cache: false,
+        url: save_url,
+        data: {
+            data_dir: data_dir,
+            file_name: file_name, // the file type should be added
+            exp_data: jsPsych.data.get().csv()
+        }
+    });
+}
