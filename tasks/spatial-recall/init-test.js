f3fb555d82c5d0cb061a83e93dd73a76a875253b
RomainPastureau <romain.pastureau@hotmail.fr>
2023-07-18 22:10:17 +0200

    Spatial Recall: Modifying xhr sending data

--- tasks/spatial-recall/init-test.js
+++ tasks/spatial-recall/init-test.js
@@ -52,5 +52,14 @@ function saveData(save_url, data_dir, file_name) {
     xhr.open('POST', save_url); // 'write_data_new.php' is the path to the php file described above.
     xhr.setRequestHeader('Content-Type', 'application/json');
     console.log("Sending to the PHP function...");
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        if(xhr.status == 200){
+            var response = JSON.parse(xhr.responseText);
+            console.log(response.success);
+        }
+    };
+    console.log(JSON.stringify({file_name: file_name, data_dir: data_dir, data: jsPsych.data.get().csv()}));
     xhr.send(JSON.stringify({file_name: file_name, data_dir: data_dir, data: jsPsych.data.get().csv()}));
 }
+
