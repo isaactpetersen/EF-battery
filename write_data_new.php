46a6f58f320f80500f5a90b7cfe1c126e4ba0880
Isaac T. Petersen <isaac-t-petersen@uiowa.edu>
2023-07-19 07:11:04 -0500

    20230719 - data saving fix and documentation

--- write_data_new.php
+++ write_data_new.php
@@ -4,13 +4,24 @@
 // Ensure error reporting is enabled to catch any potential issues
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+// Retrieve the JSON data from the JS task
 $output = json_decode(file_get_contents('php://input'), true);
 
+// Extract the data array and file details
 $data_array = $output['data'];
 $data_dir = $output['data_dir'];
 $file_name = $output['file_name'];
-$path = $data_dir.$file.".csv";
 
+// Create the file path
+$path = $data_dir.$file_name;
+
+// Ensure the directory exists or create it if necessary
+//if (!is_dir($data_dir)) {
+//    mkdir($data_dir, 0755, true);
+//}
+
+// Save the data to the file
 file_put_contents($path, $data_array);
 
 ?>
