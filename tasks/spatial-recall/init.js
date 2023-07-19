60d8b8aa4b9859598791c711ae8e03febedc9188
RomainPastureau <romain.pastureau@hotmail.fr>
2023-07-19 17:59:41 +0200

    All Tasks: Removing Override Safe Mode

--- tasks/spatial-recall/init.js
+++ tasks/spatial-recall/init.js
@@ -3,7 +3,6 @@ time_instructions = 10;
 
 //We initialize jsPsych
 var jsPsych = initJsPsych({
-    //override_safe_mode: true, //We keep this here for test purposes, as we're running the code locally for now. If we remove it, we get a warning that some jsPsych functions only work online.
     on_finish: function() {
         last_trial_data = jsPsych.data.getLastTrialData().trials[0];
         file_name = "spatial-recall";
