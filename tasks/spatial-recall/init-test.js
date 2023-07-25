ee6445cbbffb2700b72cd3ac621afe5d416837e9
RomainPastureau <romain.pastureau@hotmail.fr>
2023-07-25 22:03:46 +0200

    Spatial Recall: Firefox compatibility
    Modifying the data sending to wait for response before moving on

--- tasks/spatial-recall/init-test.js
+++ tasks/spatial-recall/init-test.js
@@ -27,29 +27,55 @@ var jsPsych = initJsPsych({
             save_url = redirect_html + "write_data_new.php"
             data_dir = redirect_html + "results/spatial-recall/"
             jsPsych.data.get().localSave("csv", file_name+extension);
+
+            // We add the task to the URL
+            redirect_html += "experiment-go-no-go-test.html";
+
+            // We add the variables that we have in the URL
+            if((window.location.href).indexOf('?') != -1) {
+
+              var variables = window.location.href.split('?')[1]; 
+              redirect_html += "?" + variables;
+
+            };
+
+            if(last_trial_data["chain"] != "false"){
+                window.location = redirect_html;
+            };    
+
         };
 
-        // We add the task to the URL
-        redirect_html += "experiment-go-no-go-test.html";
-
-        // We add the variables that we have in the URL
-        if((window.location.href).indexOf('?') != -1) {
-
-          var variables = window.location.href.split('?')[1]; 
-          redirect_html += "?" + variables;
-
-        };
-
-        if(last_trial_data["chain"] != "false"){
-            // We redirect to the next task
-            window.location = redirect_html;
-        }; 
     }
 });
 
-function saveData(save_url, data_dir, file_name, extension) {
+function saveData(save_url, data_dir, file_name, extension, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', save_url); // 'write_data_new.php' is the path to the php file described above.
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({file_name: file_name, extension: extension, data_dir: data_dir, data: jsPsych.data.get().csv()}));
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // Data was saved successfully, now trigger the callback function
+                callback();
+            } else {
+                // Handle any errors that occurred during data saving
+                console.error('Error saving data: ' + xhr.status);
+            }
+        }
+    };
+}
+
+function redirectToNextPage() {
+
+    if((window.location.href).indexOf('?') != -1) {
+        var variables = window.location.href.split('?')[1]; 
+        redirect_html += "?" + variables;
+    };
+
+    last_trial_data = jsPsych.data.getLastTrialData().trials[0];
+    if(last_trial_data["chain"] != "false"){
+        window.location = redirect_html;
+    };  
+
 }
